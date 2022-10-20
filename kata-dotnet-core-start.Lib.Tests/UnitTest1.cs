258f87ee5783750edcfdd963a4030ff3a8138dc7
Nelis Boucké <nelis@archiwise.com>
2022-10-20 15:00:04 +0200

    Initial commit after generation and adding failing test

---
+++ kata-dotnet-core-start.Lib.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace kata_dotnet_core_start.Lib.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(true, false);
+    }
+}
---
+++ kata-dotnet-core-start.Lib/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace kata_dotnet_core_start.Lib;
+public class Class1
+{
+
+}

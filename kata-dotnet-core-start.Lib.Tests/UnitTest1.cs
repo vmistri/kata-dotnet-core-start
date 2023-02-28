3d18b79f4bf6057d7d38c49b8606fccd6a3ceb6e
vmistri <vaishali.mistri@gantnerindia.com>
2023-02-28 16:12:04 +0530

    added test case

--- kata-dotnet-core-start.Lib.Tests/UnitTest1.cs
+++ kata-dotnet-core-start.Lib.Tests/UnitTest1.cs
@@ -2,9 +2,29 @@ namespace kata_dotnet_core_start.Lib.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(2)]
+    [InlineData(9)]
+    public void Given_valid_Input_When_ValidateInput_Then_Accept(int value)
     {
-        Assert.Equal(true, false);
+        // act
+        var checkInput = TicTacToe.ValidateInput(value);
+        // assert
+        Assert.Equal(checkInput,true);
     }
+    
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-2)]
+    [InlineData(10)]
+    public void Given_Invalid_Input_When_ValidateInput_Then_Reject(int value)
+    {
+        // act
+        var checkInput = TicTacToe.ValidateInput(value);
+        // assert
+        Assert.Equal(checkInput,false);
+    }
+    
+    
+    
 }

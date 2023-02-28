c06416ce977d9eb79d9e124870f3d592573b9790
vmistri <vaishali.mistri@gantnerindia.com>
2023-02-28 16:11:48 +0530

    add static method

---
+++ kata-dotnet-core-start.Lib/TicTacToe.cs
@@ -0,0 +1,10 @@
+﻿namespace kata_dotnet_core_start.Lib;
+public class TicTacToe
+{
+    public static bool ValidateInput(int value)
+    {
+        if (value >= 1 && value <= 9)
+            return true;
+        return false;
+    }
+}
